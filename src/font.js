52149be26d6e9db4718a4463b1150c658203c430
lucasreisbr <lucas@lucasreis.com.br>
2026-02-12 17:25:29 -0300

    sending

---
+++ src/font.js
@@ -0,0 +1,13 @@
+ tailwind.config = {
+            theme: {
+                extend: {
+                    fontFamily: {
+                        'poppins': ['Poppins', 'sans-serif'],
+                    },
+                    fontWeight: {
+                        'black': '900',
+                        'regular': '400',
+                    }
+                }
+            }
+        }
